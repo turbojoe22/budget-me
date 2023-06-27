4c74185070ec9291706a2cd0e77e4e79acf39632
corty2987 <cortyblackwell@gmail.com>
2023-06-26 21:26:44 -0500

    Adding project to GitHub

---
+++ src/main/java/com/myPersonalFinance/budgetme/BudgetMeApplication.java
@@ -0,0 +1,13 @@
+package com.myPersonalFinance.budgetme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myPersonalFinance/budgetme/BudgetMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myPersonalFinance.budgetme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
