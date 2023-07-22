76f2e63acec369b48caa647dd63f7f517fee32ce
marycross3 <marycradick@gmail.com>
2023-07-22 11:35:41 -0500

    welcome page

--- src/frontend/src/views/LoginPage.vue
+++ src/frontend/src/views/LoginPage.vue
@@ -1,7 +1,10 @@
 <template>
   <div>
     <h1>Login Page</h1>
-
+    <label class="label">Username</label>
+    <input type="text" class="input" />
+    <label class="label">Password</label>
+    <input type="text" class="input" />
   </div>
 </template>
 
